01102143ac6e6009940125e9eba8ec73d15b73f6
ogzhanolguncu <ogzhan11@gmail.com>
2022-02-15 19:23:51 +0300

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Box, Center, Checkbox, Flex, Text } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [propagateLevel, setPropagateLevel] = useState<Number[]>([]);
+	const [bubbledName, setBubbledName] = useState<string>();
+	const [propagationStopped, setPropagationStopped] = useState<boolean>(false);
+
+	const handleParentClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		setTimeout(() => setPropagateLevel((prevState) => [...prevState, 1]), 1600);
+		propagationStopped && event.stopPropagation();
+		setBubbledName("PARENT");
+	};
+	const handleFirstChildClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		setTimeout(() => setPropagateLevel((prevState) => [...prevState, 2]), 1300);
+		propagationStopped && event.stopPropagation();
+		setBubbledName("First Child");
+	};
+	const handleSecondChildClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		setTimeout(() => setPropagateLevel((prevState) => [...prevState, 3]), 1000);
+		propagationStopped && event.stopPropagation();
+		setBubbledName("Second Child");
+	};
+
+	return (
+		<Center bg="blackAlpha.500" h="100vh" color="white" flexDirection="column" fontSize="2rem">
+			<Flex my="5rem" gap="1rem">
+				Clicked on <Text color="blue.500"> {bubbledName}</Text>
+			</Flex>
+			<Checkbox
+				colorScheme="blue"
+				my="5rem"
+				size="lg"
+				defaultChecked={false}
+				onChange={() => {
+					setPropagationStopped((prevState) => !prevState);
+					setPropagateLevel([]);
+				}}
+			>
+				Stop Propagation
+			</Checkbox>
+			<Box
+				width="800px"
+				p="5rem"
+				onClick={(e) => {
+					return handleParentClick(e);
+				}}
+				border="3px solid blue"
+				borderRadius="10px"
+				boxShadow={propagateLevel.includes(1) ? "0px 0px 14px blue" : "initial"}
+			>
+				<Text>Parent</Text>
+				<Box
+					p="5rem"
+					onClick={(e) => {
+						return handleFirstChildClick(e);
+					}}
+					border="3px solid green"
+					borderRadius="10px"
+					boxShadow={propagateLevel.includes(2) ? "0px 0px 14px green" : "initial"}
+				>
+					<Text>First Child</Text>
+					<Box
+						p="5rem"
+						onClick={(e) => {
+							return handleSecondChildClick(e);
+						}}
+						border="3px solid red"
+						borderRadius="10px"
+						boxShadow={propagateLevel.includes(3) ? "0px 0px 14px red" : "initial"}
+					>
+						<Text>Second Child</Text>
+					</Box>
+				</Box>
+			</Box>
+		</Center>
+	);
 }
 
 export default App;
